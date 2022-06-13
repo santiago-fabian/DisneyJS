051a223fb764d4735b89b9517ba1e1c3fcb48a41
fabian santiago <santiago.hdez.fabian@gmail.com>
2022-06-13 01:38:05 -0500

    avance 13-06-2022

--- DisneyJS/Models/Pelicula.cs
+++ DisneyJS/Models/Pelicula.cs
@@ -2,5 +2,14 @@
 {
     public class Pelicula
     {
+        public int IdPelicula { get; set; }
+        public string Titulo { get; set; }
+        public string Imagen { get; set; }
+        public string FechaCreacion { get; set; }
+        public int Calificacion { get; set; }
+        public string Personajes { get; set; }
+        public string FechaRegistro { get; set; }
+        public string FechaModificacion { get; set; }
+        public bool EnUso { get; set; }
     }
 }
--- DisneyJS/database.sql
+++ DisneyJS/database.sql
@@ -1,17 +1,31 @@
-﻿CREATE DATABASE bd_disney;
-USE bd_disney;
+﻿CREATE DATABASE db_disney;
+USE db_disney;
 
 CREATE TABLE tbl_personaje
 (
     int_id int not null auto_increment,
     var_nombre varchar (150) null,
 	int_edad integer,
-	int_peso double,
+	dbl_peso double,
 	var_historia varchar (500) null,
     dt_registro timestamp null,
     dt_modificacion timestamp null,
     var_imagen longblob, 
     bol_enuso boolean not null,
     var_peliculas varchar (150) null,
+    PRIMARY KEY (int_id)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+CREATE TABLE tbl_pelicula
+( 
+    int_id int not null auto_increment,
+    var_titulo varchar (150) null,
+    var_imagen longblob,
+    dt_creacion timestamp not null,
+    int_calificacion integer,
+    var_personajes varchar (150) null,
+    dt_registro timestamp null,
+    dt_modificacion timestamp null,
+    bol_enuso boolean not null,
     PRIMARY KEY (int_id)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
